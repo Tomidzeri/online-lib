87f19494ca1238dcf0d9c9a0c587d0840cde3431
Tomidzeri <77193054+Tomidzeri@users.noreply.github.com>
2023-08-16 00:43:58 +0200

    added a PrivateRoute to login and signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
 import Login from "./services/Login";
+import PrivateRoute from "./services/PrivateRoute";
+
 import Signup from "./services/Signup";
 import Layout from "./layout/layout";
-
 function App() {
   const storedToken = localStorage.getItem("token");
   const [token, setToken] = useState(storedToken || "");
@@ -16,13 +19,21 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login setToken={handleSetToken} />} />
-        <Route path="/signup" element={<Signup setToken={setToken} />} />
+        {/* PrivateRoute component for the login and signup pages */}
+        <Route
+          path="/login"
+          element={<PrivateRoute element={<Login setToken={handleSetToken} />} authenticated={!token} />}
+        />
+        <Route
+          path="/signup"
+          element={<PrivateRoute element={<Signup setToken={handleSetToken} />} authenticated={!token} />}
+        />
+        {/* Other routes */}
         <Route
           path="/dashboard"
           element={<Layout token={token} setToken={setToken} />}
         />
-        <Route path="/" element={<Login setToken={setToken} />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
       </Routes>
     </Router>
   );
