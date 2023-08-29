c0dbb404672ff6fe8939698a660f6670bc4c73ff
Alleksej <akeksa.minic@gmail.com>
2023-08-29 17:11:04 +0200

    Promjena izgleda aplikacije, kao i formi za dodavanje bibliotekara/ucenika

--- src/components/UI/forms/UserAddForm.js
+++ src/components/UI/forms/UserAddForm.js
@@ -4,151 +4,163 @@ import libraryAPI from "../../../utils/api";
 import "./UserAddForm.css";
 
 const UserAddForm = () => {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const role = searchParams.get("role");
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const role = searchParams.get("role");
 
-  const [formData, setFormData] = useState({
-    role_id: role === "Bibliotekar" ? 1 : 2,
-    name: "",
-    surname: "",
-    jmbg: "",
-    email: "",
-    username: "",
-    password: "",
-    password_confirmation: "",
-  });
+    const [formData, setFormData] = useState({
+        role_id: role === "Bibliotekar" ? 1 : 2,
+        name: "",
+        surname: "",
+        jmbg: "",
+        email: "",
+        username: "",
+        password: "",
+        password_confirmation: "",
+    });
 
-  console.log(role);
+    console.log(role);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const token = localStorage.getItem("token");
-      const response = await libraryAPI.post("/users/store", formData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      console.log("User added successfully:", response.data);
-    } catch (error) {
-      console.error("Error adding user:", error);
-    }
-  };
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        try {
+            const token = localStorage.getItem("token");
+            const response = await libraryAPI.post("/users/store", formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log("User added successfully:", response.data);
+        } catch (error) {
+            console.error("Error adding user:", error);
+        }
+    };
 
-  return (
-    <div className="form-container">
-      <h1>Create new user</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            className="form-input"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="surname">Surname:</label>
-          <input
-            type="text"
-            id="surname"
-            name="surname"
-            value={formData.surname}
-            onChange={handleInputChange}
-            className="form-input"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="jmbg">JMBG:</label>
-          <input
-            type="text"
-            id="jmbg"
-            name="jmbg"
-            value={formData.jmbg}
-            onChange={handleInputChange}
-            className="form-input"
-            pattern="[0-9]{13}"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            className="form-input"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-            className="form-input"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            className="form-input"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password_confirmation">Confirm Password:</label>
-          <input
-            type="password"
-            id="password_confirmation"
-            name="password_confirmation"
-            value={formData.password_confirmation}
-            onChange={handleInputChange}
-            className="form-input"
-            required
-          />
-        </div>
-        <button type="submit" className="form-button">
-          Submit
-        </button>
-        {role === "Bibliotekar" ? (
-          <Link to="/librarians" className="cancel-button">
-            Cancel
-          </Link>
-        ) : (
-          <Link to="/students" className="cancel-button">
-            Cancel
-          </Link>
-        )}
-      </form>
-    </div>
-  );
+    return ( <
+        div className = "form-container" >
+        <
+        h3 className = "h2" > Create new user < /h3> <
+        form onSubmit = { handleSubmit } >
+        <
+        div className = "form-group" >
+        <
+        input type = "text"
+        id = "name"
+        name = "name"
+        placeholder = "Name"
+        value = { formData.name }
+        onChange = { handleInputChange }
+        className = "form-input"
+        required /
+        >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        input type = "text"
+        id = "surname"
+        name = "surname"
+        placeholder = "Surname"
+        value = { formData.surname }
+        onChange = { handleInputChange }
+        className = "form-input"
+        required /
+        >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        input type = "text"
+        id = "jmbg"
+        name = "jmbg"
+        placeholder = "JMBG"
+        value = { formData.jmbg }
+        onChange = { handleInputChange }
+        className = "form-input"
+        pattern = "[0-9]{13}"
+        required /
+        >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        input type = "email"
+        id = "email"
+        name = "email"
+        placeholder = "Email"
+        value = { formData.email }
+        onChange = { handleInputChange }
+        className = "form-input"
+        required /
+        >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        input type = "text"
+        id = "username"
+        name = "username"
+        placeholder = "Username"
+        value = { formData.username }
+        onChange = { handleInputChange }
+        className = "form-input"
+        required /
+        >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        input type = "password"
+        id = "password"
+        name = "password"
+        placeholder = "Password"
+        value = { formData.password }
+        onChange = { handleInputChange }
+        className = "form-input"
+        required /
+        >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        input type = "password"
+        id = "password_confirmation"
+        name = "password_confirmation"
+        placeholder = "Password confirmation"
+        value = { formData.password_confirmation }
+        onChange = { handleInputChange }
+        className = "form-input"
+        required /
+        >
+        <
+        /div> <
+        button type = "submit"
+        className = "form-button" >
+        Submit <
+        /button> {
+            role === "Bibliotekar" ? ( <
+                Link to = "/librarians"
+                className = "cancel-button" >
+                Cancel <
+                /Link>
+            ) : ( <
+                Link to = "/students"
+                className = "cancel-button" >
+                Cancel <
+                /Link>
+            )
+        } <
+        /form> <
+        /div>
+    );
 };
 
 export default UserAddForm;
-
