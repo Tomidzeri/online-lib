b1db3f5da447ecbbdbe9191ad6d295a6a2a96d14
Tomidzeri <77193054+Tomidzeri@users.noreply.github.com>
2023-09-07 18:00:15 +0200

    minor fixevi

--- src/layout/dashboard-content/Activities.js
+++ src/layout/dashboard-content/Activities.js
@@ -2,38 +2,45 @@ import React from "react";
 import List from "../../components/UI/lists/List";
 import "./Activities.css";
 
-const activitiesData = [{
-        title: "Rent",
-        author: "Robinson Kruso",
-        action: "for",
-        target: "Petar Njegos",
-        date: "12.03.2020",
-    },
-    {
-        title: "Rent",
-        author: "The shinning",
-        action: "for",
-        target: "Pero Perovic",
-        date: "21.02.2021",
-    },
-    {
-        title: "Return",
-        author: "John Doe",
-        action: "from",
-        target: "Jane Smith",
-        date: "05.09.2022",
-    },
-    {
-        title: "Exhhibiton",
-        author: "",
-        action: "Books of Stephen King",
-        target: "",
-        date: "30.11.2023",
-    },
+const activitiesData = [
+  {
+    title: "Robinson Kruso",
+    author: "Valentina K.",
+    action: "izdala knjigu",
+    target: "Petru Njegosu",
+    date: "12.03.2020",
+  },
+  {
+    title: "Tom Sojer",
+    author: "Valentina K.",
+    action: "izdala knjigu",
+    target: "Peru Perovicu",
+    date: "21.02.2021",
+  },
+  {
+    title: "Lorem Ipsum",
+    author: "John Doe",
+    action: "created content",
+    target: "Jane Smith",
+    date: "05.09.2022",
+  },
+  {
+    title: "Example Event",
+    author: "Alice Johnson",
+    action: "scheduled event",
+    target: "Bob Williams",
+    date: "30.11.2023",
+  },
+  {
+    title: "Project Completion",
+    author: "Sam Wilson",
+    action: "completed project",
+    target: "Jennifer Adams",
+    date: "18.09.2025",
+  },
 ];
 
 const Activities = () => {
-<<<<<<< HEAD:src/layout/dashboard-content/Activities.js
   const activityListItems = activitiesData.map((activity, index) => (
     <div key={index} className="activity-item">
       <div className="activity-header">
@@ -53,35 +60,6 @@ const Activities = () => {
       <button>Show</button>
     </div>
   );
-=======
-    const activityListItems = activitiesData.map((activity, index) => ( <
-        ul className = "activity-list" >
-        <
-        li key = { index }
-        className = "activity-item" >
-        <
-        div className = "activity-header" >
-        <
-        span className = "activity-title" > { activity.title } < /span> <
-        span className = "activity-date" > { activity.date } < /span> <
-        /div> <
-        div className = "activity-details" > { `${activity.author} ${activity.action} ${activity.target}` } <
-        /div> <
-        /li> <
-        /ul>
-    ));
-
-    return ( <
-        div className = "activities-container" >
-        <
-        h3 className = "activities-title" > Activities < /h3> <
-        List items = { activityListItems }
-        className = "custom-list" / >
-        <
-        button className = "button" > Show More < /button> <
-        /div>
-    );
->>>>>>> eb74f6a79c9477e4435551fb532fd862c09954af:src/layout/dashboard/Activities.js
 };
 
 export default Activities;
