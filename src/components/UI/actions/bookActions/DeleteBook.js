64d71c8b956ae6aaf95fec0103490fb1a2c0c846
Tomidzeri <77193054+Tomidzeri@users.noreply.github.com>
2023-09-28 00:18:30 +0200

    dodate popup konfirmacije na delete handlere

--- src/components/UI/actions/bookActions/DeleteBook.js
+++ src/components/UI/actions/bookActions/DeleteBook.js
@@ -1,17 +1,55 @@
 import React from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import useDeleteBook from "../../../../queries/knjige/useDeleteBook";
+import { TiTick } from "react-icons/ti";
+import { GiCrossMark } from "react-icons/gi";
 
 const DeleteBook = ({ book, onDelete }) => {
-  const deleteBook = useDeleteBook(); 
+  const deleteBook = useDeleteBook();
 
   const handleDelete = () => {
-    deleteBook(book.id); 
-    onDelete(book.id); 
+    toast.warning(
+      <>
+        <div style={{ textAlign: "center" }}>
+          <p>Da li ste sigurni da zelite da izbrisete knjigu:</p>
+          <p>
+            <span style={{ fontWeight: "bold", textAlign: "center" }}>
+              {book.title}
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-row justify-between">
+          <button
+            onClick={() => {
+              deleteBook(book.id);
+              onDelete(book.id);
+              toast.dismiss();
+            }}
+            style={{ color: "red" }}
+          >
+            <TiTick />
+          </button>
+          <button
+            onClick={() => {
+              toast.dismiss();
+            }}
+            style={{ color: "blue" }}
+          >
+            <GiCrossMark />
+          </button>
+        </div>
+      </>,
+      {
+        autoClose: false,
+        closeOnClick: false,
+        closeButton: false,
+      }
+    );
   };
 
   return (
     <div>
-      {/* <p>Are you sure you want to delete {book.name}?</p> */}
       <button onClick={handleDelete}>Delete book</button>
     </div>
   );
--- src/components/UI/actions/userActions/DeleteUser.js
+++ src/components/UI/actions/userActions/DeleteUser.js
@@ -1,18 +1,56 @@
 import React from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import useDeleteUser from "../../../../queries/korisnici/useDeleteUser";
+import { TiTick } from "react-icons/ti";
+import { GiCrossMark } from "react-icons/gi";
 import './DeleteUser.css';
 
 const DeleteUser = ({ user, onDelete }) => {
   const deleteUser = useDeleteUser();
 
   const handleDelete = () => {
-    deleteUser(user.id);
-    onDelete(user.id);
+    toast.warning(
+      <>
+        <div style={{ textAlign: "center" }}>
+          <p>Da li ste sigurni da zelite da izbrisete korisnika:</p>
+          <p>
+            <span style={{ fontWeight: "bold", textAlign: "center" }}>
+              {user.name} {user.surname}
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-row justify-between">
+          <button
+            onClick={() => {
+              deleteUser(user.id);
+              onDelete(user.id);
+              toast.dismiss();
+            }}
+            style={{ color: "red" }}
+          >
+            <TiTick />
+          </button>
+          <button
+            onClick={() => {
+              toast.dismiss();
+            }}
+            style={{ color: "blue" }}
+          >
+            <GiCrossMark />
+          </button>
+        </div>
+      </>,
+      {
+        autoClose: false,
+        closeOnClick: false,
+        closeButton: false,
+      }
+    );
   };
 
   return (
     <div className="delete">
-      {/* <p>Are you sure you want to delete {user.name}?</p> */}
       <button onClick={handleDelete}>Delete user</button>
     </div>
   );
