8aef4076c438ec52ab0244d47f496e912d55e64b
Tomidzeri <77193054+Tomidzeri@users.noreply.github.com>
2023-10-03 16:22:13 +0200

    minor fixes

--- src/layout/Dashboard.js
+++ src/layout/Dashboard.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Lottie from "react-lottie";
-import animationData from "./animation.json"; 
+import animationData from "./animation.json";
 import Activities from "./dashboard-content/Activities";
 import Statistics from "./dashboard-content/Statistics";
 import Reservations from "./dashboard-content/Reservations";
 import "./dashboard-content/Dashboard.css";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 2000); 
+    }, 2000);
   }, []);
 
   const defaultOptions = {
     loop: false,
     autoplay: true,
-    animationData: animationData, 
+    animationData: animationData,
   };
 
   const animationStyle = {
@@ -26,9 +28,13 @@ const Dashboard = () => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: "300px", 
-    height: "300px", 
-    backgroundColor: "transparent", 
+    width: "300px",
+    height: "300px",
+    backgroundColor: "transparent",
+  };
+
+  const navigateToActivities = () => {
+    navigate("/activities");
   };
 
   return (
@@ -43,6 +49,7 @@ const Dashboard = () => {
         <>
           <div className="left-pane">
             <Activities />
+            <button onClick={navigateToActivities}>Pokazi vise</button>
           </div>
           <div className="right-pane flex flex-col">
             <Reservations />
